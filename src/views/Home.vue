689d0b8edab04319745238843d66fbbec41c2bf8
Nicolas Duplaix <nicolas@widop.com>
2020-02-11 22:40:29 +0100

    [FEAT] homepage banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="container">
     <h1 class="text-center">Bienvenue sur Renarou</h1>
-    <banner class="mt-5" image-link="/Banners/banniere-nature.jpg" image-alt="Bannière" />
+    <images-carousel/>
     <product-banner class="mt-3" title="Nouveautés" :products="newProducts" />
     <product-banner class="mt-3" title="Articles en promo" :products="discountProducts" />
   </div>
 </template>
 
 <script>
-import { Banner, ProductBanner } from "../components/Banner";
+import { ImagesCarousel, ProductBanner } from "../components/Banner";
 import { mapGetters } from "vuex";
 
 export default {
   components: {
-    Banner,
+    ImagesCarousel,
     ProductBanner
   },
   computed: {
